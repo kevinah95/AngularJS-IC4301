057a8deff6c3cadb084c52bbea7db30a20883236
jm95aguilar <jm95aguilar@hotmail.com>
2015-05-11 06:50:54 -0600

    Archivos de Ejemplo

---
+++ myAppCtrl.js
@@ -0,0 +1,45 @@
+var app = angular.module('MyApp', []);
+app.controller('questionController', function($scope){
+   //objeto pregunta
+   $scope.pregunta = {
+      id: 1,
+      premisa: '¿Qué tipo de mascota preferiría?',
+      respuestas: [
+         {
+            id: 1,
+            text: 'Perro',
+            active: 'false'
+         },
+         {
+            id: 2,
+            text: 'Gato',
+            active: 'false'
+         },
+         {
+            id: 3,
+            text: 'Conejo',
+            active: 'false'
+         },
+         
+      ]
+   }
+
+   $scope.marcar = function(){
+	   angular.forEach($scope.pregunta.respuestas, function(value, key) {
+	      value.active = false;
+	   });
+	   this.respuesta.active = true;
+	}
+
+	$scope.respuestas = [];
+
+    $scope.responder = function(){
+      angular.forEach($scope.pregunta.respuestas, function(respuesta, i) {
+         if (respuesta.active)
+            $scope.respuestas.push({ id:$scope.id, key:respuesta.id });
+      });
+    };
+
+});
+
+
